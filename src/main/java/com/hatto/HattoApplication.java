b55e2375ed8105ee660da391e3a16406414ebdb6
sejinmac <lsjyahoo@gmail.com>
2026-02-27 13:21:15 +0900

    Initial commit

---
+++ src/main/java/com/hatto/HattoApplication.java
@@ -0,0 +1,13 @@
+package com.hatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HattoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HattoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hatto/HattoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HattoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

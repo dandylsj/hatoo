6972be8315d701437e40c8abf547e20f0f4e658c
dandylsj <lsjyahoo@gmail.com>
2026-03-24 23:39:24 +0900

    runner 테스트용1

--- src/main/java/com/hatto/config/SecurityConfig.java
+++ src/main/java/com/hatto/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*"); // 허용할 Origin (도메인)
+        configuration.addAllowedOriginPattern("*"); // 허용할 Origin (도메인).
         configuration.addAllowedMethod("*"); // 허용할 HTTP Method (GET, POST, PATCH 등)
         configuration.addAllowedHeader("*"); // 허용할 Header (Authorization 포함)
         configuration.setAllowCredentials(true);

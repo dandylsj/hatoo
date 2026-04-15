276670b3e9fb8cd4f53ec69a19dbd7be0e71bdc9
sejinmac <lsjyahoo@gmail.com>
2026-04-15 11:08:27 +0900

    ♻ refactor: 회원가입시 기본그룹 프로필이미지 저장 수정 2

--- src/main/java/com/hatoo/domain/user/UserService.java
+++ src/main/java/com/hatoo/domain/user/UserService.java
@@ -77,7 +77,17 @@ public class UserService {
                     request.getFcmToken()
             );
 
-            // 6. 응답 DTO 생성 반환
+            // 6. 프로필 이미지가 변경된 경우, profileImg가 null인 GroupMember에도 반영
+            if (request.getProfileImg() != null) {
+                List<GroupMember> groupMembers = groupMemberRepository.findByUserId(user.getId());
+                for (GroupMember gm : groupMembers) {
+                    if (gm.getProfileImg() == null) {
+                        gm.updateProfileImg(request.getProfileImg());
+                    }
+                }
+            }
+
+            // 7. 응답 DTO 생성 반환
             return new UserInfoModifyResponse(user.getId(), user.getNickname(), user.getProfileImg());
     }
 

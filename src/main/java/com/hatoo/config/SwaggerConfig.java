e69913ad2f177c75a56bf1f7bfea8dfb2fec3653
dandylsj <lsjyahoo@gmail.com>
2026-06-11 17:16:58 +0900

    ✨ feat: Oauth 로그인시 중복되는 이메일을 사용한 로그인을 할 경우 예외처리 추가

--- src/main/java/com/hatoo/config/SwaggerConfig.java
+++ src/main/java/com/hatoo/config/SwaggerConfig.java
@@ -58,7 +58,7 @@ public class SwaggerConfig {
 
         // Ubuntu 서버
         Server ubuntuServer = new Server();
-        ubuntuServer.setUrl("https://dandhomelab.uk");
+        ubuntuServer.setUrl("https://dandyhomelab.uk");
         ubuntuServer.setDescription("Hatoo Ubuntu Server");
 
         return new OpenAPI()
